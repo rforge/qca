29fc76bbf2cd70829949a3a10dff5dcd621e1c1b
dusadrian <dusadrian@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-26 13:55:54 +0000

    Revert to the old pkg structure
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qca@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- QCA/R/fuzzyor.R
+++
@@ -1,9 +0,0 @@
-`fuzzyor` <- function(...) {
-    other.args <- list(...)
-    if (is.matrix(other.args[[1]]) | is.data.frame(other.args[[1]])) {
-        return(apply(other.args[[1]], 1, max))
-    }
-    else {
-        return(pmax(...))
-    }
-}
