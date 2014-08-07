4f23441bafa4df56917be0dccff6e35a9e4bb97f
alrikthiem <alrikthiem@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-07 08:24:59 +0000

    Inserted "title" from DESCRIPTION after change of Title in DESCRIPTION
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qca@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/onAttach.R
+++ pkg/R/onAttach.R
@@ -2,12 +2,14 @@
     
     meta <- packageDescription("QCA")
     year <- sub("-.*", "", meta$Date)
-    note <- sprintf("R package version %s", meta$Version)
+    title <- meta$Title
+    note <- sprintf("R Package Version %s.", meta$Version)
     
     msg <- paste("Dusa, Adrian and Alrik Thiem", sprintf("(%s).", year),
-                 "QCA: A Package for Qualitative Comparative Analysis.",
-                  paste(note, ".", sep = ""),
-                  "URL: http://cran.r-project.org/package=QCA")
-    msg <- paste(strwrap(msg, indent=2, exdent=2), collapse="\n")
-    packageStartupMessage("\nPlease cite the QCA package as:\n", msg, "\n\nA complete BibTeX reference is provided by:\n  citation(\"QCA\")\n")
+                 sprintf("%s.", title), note,
+                  "URL: http://cran.r-project.org/package=QCA.")
+    
+    msg <- paste(strwrap(msg, indent = 2, exdent = 2), collapse = "\n")
+    
+    packageStartupMessage("\nPlease cite the QCA package as:\n\n", msg, "\n\nA BibTeX entry is provided by:\n  citation(\"QCA\")\n")
 }
