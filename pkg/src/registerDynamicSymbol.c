1183229abbfdda82702a671c1330639278407303
dusadrian <dusadrian@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-08 13:53:18 +0000

    changed to allow building on R-Forge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qca@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/registerDynamicSymbol.c
+++ pkg/src/registerDynamicSymbol.c
@@ -27,8 +27,41 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <R.h>
 #include <Rinternals.h>
+#include <stdlib.h> 
 #include <R_ext/Rdynload.h>
-void R_init_markovchain(DllInfo* info) {
-  R_registerRoutines(info, NULL, NULL, NULL, NULL);
-  R_useDynamicSymbols(info, TRUE);
+extern SEXP ccubes(SEXP);
+extern SEXP combinations(SEXP);
+extern SEXP createMatrix(SEXP);
+extern SEXP findmin(SEXP);
+extern SEXP findSubsets(SEXP, SEXP, SEXP, SEXP);
+extern SEXP getRow(SEXP);
+extern SEXP QMC(SEXP, SEXP);
+extern SEXP removeRedundants(SEXP, SEXP, SEXP);
+extern SEXP setColnames(SEXP, SEXP);
+extern SEXP setDimnames(SEXP, SEXP);
+extern SEXP setRownames(SEXP, SEXP);
+extern SEXP solveChart(SEXP, SEXP, SEXP);
+extern SEXP superSubset(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP truthTable(SEXP, SEXP, SEXP, SEXP);
+static const R_CallMethodDef CallEntries[] = {
+    {"ccubes",           (DL_FUNC) &ccubes,           1},
+    {"combinations",     (DL_FUNC) &combinations,     1},
+    {"createMatrix",     (DL_FUNC) &createMatrix,     1},
+    {"findmin",          (DL_FUNC) &findmin,          1},
+    {"findSubsets",      (DL_FUNC) &findSubsets,      4},
+    {"getRow",           (DL_FUNC) &getRow,           1},
+    {"QMC",              (DL_FUNC) &QMC,              2},
+    {"removeRedundants", (DL_FUNC) &removeRedundants, 3},
+    {"setColnames",      (DL_FUNC) &setColnames,      2},
+    {"setDimnames",      (DL_FUNC) &setDimnames,      2},
+    {"setRownames",      (DL_FUNC) &setRownames,      2},
+    {"solveChart",       (DL_FUNC) &solveChart,       3},
+    {"superSubset",      (DL_FUNC) &superSubset,      8},
+    {"truthTable",       (DL_FUNC) &truthTable,       4},
+    {NULL, NULL, 0}
+};
+void R_init_QCA(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
